ab461bb73d4da8a67556115711ef344cfbff9ea6
kennedy <72859935+guruhkennedy@users.noreply.github.com>
2024-03-13 09:18:22 +0700

    Update config.js

--- database/config.js
+++ database/config.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   port     : '3306',
   user     : 'root',
   password : '',
-  database : 'jsp_eCommerce'
+  database : ''
 });
 
 connection.connect(function(err) {
