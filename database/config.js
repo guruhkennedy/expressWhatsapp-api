aa38edcf469cc6530ddcf72db643c09838f1c362
guruhkennedy <guruh52480@gmail.com>
2024-03-13 08:42:41 +0700

    versi 1

---
+++ database/config.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  port     : '3306',
+  user     : 'root',
+  password : '',
+  database : 'jsp_eCommerce'
+});
+
+connection.connect(function(err) {
+    if (err){
+      console.log(err);
+      //throw err;
+    } else {
+      console.log('DB connected :)');
+    }
+});
+
+module.exports = connection;
